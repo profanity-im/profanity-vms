cc393721036f86ace1f65ae3eddf14414d75389c
James Booth <boothj5@gmail.com>
2015-07-09 00:46:40 +0100

    Added centos 7 64bit

---
+++ centos/provisioning/prof-install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+. /tmp/errhandler.sh
+. /tmp/libstrophe.sh
+. /tmp/cmocka.sh
+. /tmp/profanity.sh
+
+ARCH=`arch`
+yum -y install git
+yum -y install gcc autoconf automake cmake
+yum -y install openssl-devel.$ARCH expat-devel.$ARCH ncurses-devel.$ARCH  glib2-devel.$ARCH libnotify-devel.$ARCH libcurl-devel.$ARCH libXScrnSaver-devel.$ARCH libotr3-devel.$ARCH readline-devel.$ARCH libtool libuuid-devel.$ARCH
+
+mkdir projects-git
+cd projects-git
+
+install_libstrophe /usr
+install_cmocka
+/sbin/ldconfig
+
+install_profanity
