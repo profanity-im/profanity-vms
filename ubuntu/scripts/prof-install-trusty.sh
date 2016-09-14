b504ebaffb85cba39bbad512593e995c3ac84eb0
James Booth <boothj5@gmail.com>
2016-09-14 21:26:26 +0100

    Update Ubuntu VMs

--- ubuntu/scripts/prof-install-trusty.sh
+++ ubuntu/scripts/prof-install-trusty.sh
@@ -2,17 +2,19 @@
 
 . /tmp/errhandler.sh
 . /tmp/libstrophe.sh
+. /tmp/libmesode.sh
 . /tmp/cmocka.sh
 . /tmp/profanity.sh
 
 apt-get -y install git
-apt-get -y install autoconf automake libtool cmake
-apt-get -y install libssl-dev libexpat1-dev libncursesw5-dev libglib2.0-dev libnotify-dev libcurl3-dev libxss-dev libotr5-dev libreadline-dev libgpgme11-dev uuid-dev 
+apt-get -y install autoconf automake libtool cmake autoconf-archive
+apt-get -y install libssl-dev libexpat1-dev libncursesw5-dev libglib2.0-dev libnotify-dev libcurl3-dev libxss-dev libotr5-dev libreadline-dev libgpgme11-dev uuid-dev libgtk2.0-dev
+apt-get -y install python-dev
 
 mkdir projects-git
 cd projects-git
 
-install_libstrophe --prefix=/usr
+install_libmesode --prefix=/usr
 install_cmocka
 
 install_profanity
