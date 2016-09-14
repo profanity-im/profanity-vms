b0e9babe84435819658cd4e3d216234637a36024
James Booth <boothj5@gmail.com>
2016-09-14 01:39:53 +0100

    Update OpenSuse VMs

--- opensuse/scripts/prof-install.sh
+++ opensuse/scripts/prof-install.sh
@@ -2,17 +2,19 @@
 
 . /tmp/errhandler.sh
 . /tmp/libstrophe.sh
+. /tmp/libmesode.sh
 . /tmp/cmocka.sh
 . /tmp/profanity.sh
 
 zypper -n in git
-zypper -n in gcc automake make autoconf cmake
-zypper -n in gcc libopenssl-devel expat libexpat-devel ncurses-devel glib2-devel libnotify-devel libcurl-devel libXScrnSaver-devel libotr-devel readline-devel libtool libuuid-devel libgpgme-devel
+zypper -n in gcc automake make autoconf cmake autoconf-archive
+zypper -n in gcc libopenssl-devel expat libexpat-devel ncurses-devel glib2-devel libnotify-devel libcurl-devel libXScrnSaver-devel libotr-devel readline-devel libtool libuuid-devel libgpgme-devel gtk2-devel
+zypper -n in python-devel
 
 mkdir projects-git
 cd projects-git
 
-install_libstrophe --prefix=/usr
+install_libmesode --prefix=/usr/local
 install_cmocka
 /sbin/ldconfig
 
